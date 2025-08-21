f229bcc265f65c108b90142aed1c6ba496f213f1
heinrob <github@petrosh.de>
2025-08-21 20:32:07 +0200

    WIP theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import type { Metadata } from "next";
 import Link from "next/link";
+import { ThemeProvider } from "next-themes";
 import HouseSVG from '@/app/svg/house';
 import styles from '@/app/styles/App.module.css';
 import "@/app/globals.css";
 import Search from "@/app/components/Search";
 import { SearchProvider } from "@/app/context/search";
+import ThemeToggle from "./components/ThemeToggle";
 
 export const metadata: Metadata = {
   title: "Recipe Web",
@@ -17,26 +19,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <SearchProvider>
-          <header>
-            <div className={styles['header-wrapper']}>
-              <div className={styles['header-left']}>
-                <Link href="/" className={styles.home}>
-                  <HouseSVG />
-                </Link>
+        <ThemeProvider>
+          <SearchProvider>
+            <header>
+              <div className={styles['header-wrapper']}>
+                <div className={styles['header-left']}>
+                  <Link href="/" className={styles.home}>
+                    <HouseSVG />
+                  </Link>
+                  <ThemeToggle />
+                </div>
+                <span className={styles.title}>Recipe Web</span>
+                <div className={styles['search-wrapper']}>
+                  <Search />
+                </div>
               </div>
-              <span className={styles.title}>Recipe Web</span>
-              <div className={styles['search-wrapper']}>
-                <Search />
-              </div>
-            </div>
-          </header>
-          <main>{children}</main>
-          <footer className={styles.footer}>©2025, Robin Heinbockel<br />recipes provided by the respective GitHub users</footer>
-        </SearchProvider>
+            </header>
+            <main>{children}</main>
+            <footer className={styles.footer}>©2025, Robin Heinbockel<br />recipes provided by the respective GitHub users</footer>
+          </SearchProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
